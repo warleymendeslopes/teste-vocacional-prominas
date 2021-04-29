d16c699583fd2550b852a3a1dd4154e614288580
Warley Mendes Lopes <warleymendesdev@gmail.com>
2021-04-29 16:00:43 -0300

    title formulario

---
+++ components/formularioinicial.js
@@ -0,0 +1,10 @@
+export default function forms() {
+  return (
+<>
+    <div class="content-formulario">
+        <h1>Descubra qual Pós-Graduação fazer!</h1>
+        <span>Faça o nosso teste vocacional e descubra qual curso se encaixa melhor no seu perfil</span>
+    </div>
+</>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from '../components/header'
 import Nav from '../components/nav'
 import Image from 'next/image'
+import Formulario from '../components/formularioinicial'
+
 
 
 export default function Home() {
@@ -9,18 +11,32 @@ export default function Home() {
 
       <Head />
       <Nav />
+      <Formulario />
+
+
+
+
 
 
       <div class="imgavatar">
-            <Image src="/avatar.png" alt="Aluno satisfeito com teste vocacional do Grupo Prominas" width={900} height={900} />
-        </div>
+        <Image src="/avatar.png" alt="Aluno satisfeito com teste vocacional do Grupo Prominas" width={1200} height={900} />
+      </div>
+      <div id="content-grid">
+        <div class="lado1"></div>
+        <div class="lado2"></div>
+      </div>
 
       <style jsx>{`
-                      .imgavatar {
-                        position: absolute !important;
-                        bottom: 0px !important;
-                        left:5% !important;
-                    }
+      #content-grid{
+
+      }
+
+      .imgavatar {
+        position: fixed !important;
+        bottom: -4px !important;
+      }
+
+
     
     `}</style>
     </>
